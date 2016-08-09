4d7d002b3f37f59319560fbea80d37916d0a5ada
Mint-Zhao-Chiu <mint.zhao.chiu@gmail.com>
2016-08-09 22:41:55 +0800

    add storage module for farmer accounts

---
+++ account/store/rocksdb_store_test.go
@@ -0,0 +1,85 @@
+/*
+Copyright Mojing Inc. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package store
+
+import (
+	"fmt"
+	"gopkg.in/check.v1"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+type RocksdbStorageTest struct {
+	storage *RocksdbStorage
+}
+
+var _ = check.Suite(&RocksdbStorageTest{})
+
+func (t *RocksdbStorageTest) SetUpSuite(c *check.C) {
+	var err error
+	t.storage, err = NewRocksdbStorage(filepath.Join(os.TempDir(), "/test.db"))
+	c.Assert(err, check.IsNil)
+}
+
+func (t *RocksdbStorageTest) TearDownSuite(c *check.C) {
+	t.storage.Close()
+	os.Remove(filepath.Join(os.TempDir(), "/test.db"))
+}
+
+func (t *RocksdbStorageTest) TestRocksdbStorage_Set_Get(c *check.C) {
+	c.Assert(t.storage.Set([]byte("abc"), []byte("abc")), check.IsNil)
+	get, err := t.storage.Get([]byte("abc"))
+	c.Assert(err, check.IsNil)
+	c.Assert(string(get), check.Equals, "abc")
+}
+
+func (t *RocksdbStorageTest) TestRocksdbStorage_Del(c *check.C) {
+	c.Assert(t.storage.Set([]byte("abc"), []byte("abc")), check.IsNil)
+	c.Assert(t.storage.Del([]byte("abc")), check.IsNil)
+	get, err := t.storage.Get([]byte("abc"))
+	c.Logf("after delete, get value: %v, err: %v", string(get), err)
+	c.Assert(string(get), check.Equals, "")
+}
+
+func (t *RocksdbStorageTest) BenchmarkRocksdbStorage_Set(c *check.C) {
+	for i := 0; i < c.N; i++ {
+		val := []byte(fmt.Sprintf("benchmarkRocksdb_%v", i))
+		t.storage.Set(val, val)
+	}
+}
+
+func (t *RocksdbStorageTest) BenchmarkRocksdbStorage_Get(c *check.C) {
+	val := []byte("benchmarkRocksdb")
+	t.storage.Set(val, val)
+
+	for i := 0; i < c.N; i++ {
+		t.storage.Get(val)
+	}
+}
+
+func (t *RocksdbStorageTest) BenchmarkRocksdbStorage_Del(c *check.C) {
+	val := []byte("benchmarkRocksdb")
+	t.storage.Set(val, val)
+
+	for i := 0; i < c.N; i++ {
+		t.storage.Del(val)
+	}
+}
---
+++ account/store/store.go
@@ -0,0 +1,97 @@
+/*
+Copyright Mojing Inc. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package store
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type Storage interface {
+	Get([]byte) ([]byte, error)
+	Set([]byte, []byte) error
+	Del([]byte) error
+}
+
+func NewStore(backend string, args ...interface{}) (storage Storage, err error) {
+	if backend == "" {
+		backend = "rocksdb"
+	}
+
+	switch backend {
+	case "rocksdb":
+		if len(args) < 1 {
+			err = fmt.Errorf("rocksdb set up at least need a db path")
+			return
+		}
+		storage, err = NewRocksdbStorage(args[0].(string))
+	default:
+		err = fmt.Errorf("not supported backend: %v", backend)
+	}
+
+	return
+}
+
+func dirMissingOrEmpty(path string) (bool, error) {
+	dirExists, err := dirExists(path)
+	if err != nil {
+		return false, err
+	}
+	if !dirExists {
+		return true, nil
+	}
+
+	dirEmpty, err := dirEmpty(path)
+	if err != nil {
+		return false, err
+	}
+	if dirEmpty {
+		return true, nil
+	}
+	return false, nil
+}
+
+func dirExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func dirEmpty(path string) (bool, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.Readdir(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err
+}
+
+func makeCopy(src []byte) []byte {
+	dest := make([]byte, len(src))
+	copy(dest, src)
+	return dest
+}
