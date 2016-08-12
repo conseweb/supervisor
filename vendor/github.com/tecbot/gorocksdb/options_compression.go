8947b4890f69a5df157e6a1ea44f8c6553c2aab6
Mint-Zhao-Chiu <mint.zhao.chiu@gmail.com>
2016-08-12 13:40:59 +0800

    change vender reference

--- vendor/github.com/tecbot/gorocksdb/options_compression.go
+++ vendor/github.com/tecbot/gorocksdb/options_compression.go
@@ -2,23 +2,21 @@ package gorocksdb
 
 // CompressionOptions represents options for different compression algorithms like Zlib.
 type CompressionOptions struct {
-	WindowBits   int
-	Level        int
-	Strategy     int
-	MaxDictBytes int
+	WindowBits int
+	Level      int
+	Strategy   int
 }
 
 // NewDefaultCompressionOptions creates a default CompressionOptions object.
 func NewDefaultCompressionOptions() *CompressionOptions {
-	return NewCompressionOptions(-14, -1, 0, 0)
+	return NewCompressionOptions(-14, -1, 0)
 }
 
 // NewCompressionOptions creates a CompressionOptions object.
-func NewCompressionOptions(windowBits, level, strategy, maxDictBytes int) *CompressionOptions {
+func NewCompressionOptions(windowBits, level, strategy int) *CompressionOptions {
 	return &CompressionOptions{
-		WindowBits:   windowBits,
-		Level:        level,
-		Strategy:     strategy,
-		MaxDictBytes: maxDictBytes,
+		WindowBits: windowBits,
+		Level:      level,
+		Strategy:   strategy,
 	}
 }
